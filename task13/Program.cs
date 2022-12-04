b134b2d299494d41849da2cee13b1f91e6579b4d
Nikita <nikita.serg.utkin@gmail.com>
2022-12-05 04:09:02 +0900

    Работает13

---
+++ task13/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int d = 0, k = 0;
+if (number > 99)
+{
+    k = number;
+    d = k % 10;
+    while (k >= 1000)
+    {
+        k /= 10;
+        d = k % 10;
+    }
+Console.WriteLine($"третья цифра {d}");
+}
+else
+{
+    Console.WriteLine("третьей цифры нет");
+}
